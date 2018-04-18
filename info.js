32f7cc180c9569f1844f3bd5bd604c72f2f5a8c4
KatRapp <35379592+KatRapp@users.noreply.github.com>
2018-04-18 12:45:10 -0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,12 @@
+
+var name = "Kathrine Rapp";
+var date = new Date();
+document.write("<span id ='footer'> Kathrine's Website</span><br/>");
+
+document.write("Basic Website");
+document.write("<br>");
+document.write("2018 Kathrine Rapp");
+document.write("<br>");
+document.write("April 18 2018");
+document.write("<br>");
+document.write(Date());
